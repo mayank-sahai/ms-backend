18ead13dbf9fd7b700bf2b2649ca571cbc64489f
Mayank Sahai <mayank.ram22@gmail.com>
2024-04-16 22:43:42 +0530

    implemented joi, winston, morgan, config using dotenv

---
+++ src/config/logger.js
@@ -0,0 +1,18 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ dirname: './logs', filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ dirname: './logs', filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+export default logger;
--- src/server.js
+++ src/server.js
@@ -1,32 +1,40 @@
-import express from 'express';
-import helmet from 'helmet';
-import compression from 'compression';
-import cors from 'cors';
-const app = express();
-const port = 3000;
-
-app.use(express.urlencoded({ extended: false }));
+import express from 'express'
+import helmet from 'helmet'
+import compression from 'compression'
+import cors from 'cors'
+import morgan from 'morgan'
+import config from './config/config.js'
+import logger from './config/logger.js'
+const app = express()
+const port = 3000
 
 // set security headers
-app.use(helmet());
+app.use(helmet())
 
 // parse json request body
-app.use(express.json());
-
-// parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // gzip compression
-app.use(compression());
+app.use(compression())
 
 // enable cors
-app.use(cors());
-app.options('*', cors());
+app.use(cors())
+app.options('*', cors())
+
+// Logging with Morgan
+if (config.env === 'production') {
+    app.use(morgan('combined', { stream: logger.stream }))
+} else {
+    app.use(morgan('dev'))
+}
 
 app.get('/ping', (req, res) => {
-  res.send('Hello World!');
-});
+    logger.info('ping requested')
+    res.send('Hello World!')
+    logger.info('ping exiting')
+})
 
-app.listen(port, () => {
-  console.log(`Backend app listening on port ${port}`);
-});
+app.listen(config.port, () => {
+    logger.info(`Backend app listening on port ${config.port}`)
+})
